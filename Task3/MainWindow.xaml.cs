7949fd29c4450d60f0a80c4f92af34abf5480fef
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:46:49 +0300

    Initial commit

---
+++ Task3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BirthDatePicker.DisplayDateEnd = DateTime.Now;
+        }
+
+        private void BirthDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            YOLabel.Content = $"Birthday ({DateTime.Now.Year - BirthDatePicker.SelectedDate.Value.Year} full years)";
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTextBox.Text) || string.IsNullOrEmpty(PriamryPasswordBox.Password) || PriamryPasswordBox.Password != ConfirmPasswordBox.Password)
+                MessageBox.Show("Incorrect data");
+            else
+                MessageBox.Show($"{LoginTextBox.Text}, you are signed up");
+        }
+    }
+}
---
+++ Task4/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Task4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PercentLabel.Content = PercentSlider.Value;
+            TotalLable.Content = TotalSlider.Value;
+        }
+
+        private void TotalSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TotalLable.Content = Math.Round(TotalSlider.Value, 2);
+            ResultsLabel.Content = Math.Round(PercentSlider.Value/100 * TotalSlider.Value, 2);
+
+        }
+
+        private void PercentSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            PercentLabel.Content = Math.Round(PercentSlider.Value, 2) + "%";
+            ResultsLabel.Content = Math.Round(PercentSlider.Value/100 * TotalSlider.Value, 2);
+        }
+    }
+}
